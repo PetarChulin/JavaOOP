b96209415e0ba90dad1be3466d6ff05704960807
PetarChulin <petercbg@mail.bg>
2022-02-28 14:24:45 +0200

    Add files via upload

---
+++ EncapsulationExercises/Problem3ShoppingSpree/Person.java
@@ -0,0 +1,53 @@
+package Problem3ShoppingSpree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Person {
+
+    private String name;
+    private double money;
+    private List<Product> products;
+
+    public Person(String name, double money) {
+        this.setName(name);
+        this.setMoney(money);
+        this.products = new ArrayList<>();
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (name.trim().isEmpty()) {
+            throw new IllegalArgumentException("Name cannot be empty");
+        }
+        this.name = name;
+    }
+
+    public double getMoney() {
+        return money;
+    }
+
+    public void setMoney(double money) {
+        if (money < 0) {
+            throw new IllegalArgumentException("Money cannot be negative");
+        }
+        this.money = money;
+    }
+
+    public void buyProduct(Product product) {
+        if (this.money >= product.getCost()) {
+            products.add(product);
+            money -= product.getCost();
+            System.out.printf("%s bought %s\n" , this.name , product.getName());
+        } else {
+            throw new IllegalArgumentException(this.name + " can't afford " + product.getName());
+        }
+    }
+}
