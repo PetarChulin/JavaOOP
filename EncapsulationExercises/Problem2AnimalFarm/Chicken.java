59d3ee3cb5edc4f424a1d11359ceb14686b0e779
PetarChulin <petercbg@mail.bg>
2022-02-28 11:48:31 +0200

    Add files via upload

---
+++ EncapsulationExercises/Problem2AnimalFarm/Chicken.java
@@ -0,0 +1,46 @@
+package EncapsulationExercises.Problem2AnimalFarm;
+
+public class Chicken {
+
+    private String name;
+    private int age;
+
+    public Chicken(String name, int age) {
+        this.setName(name);
+        this.setAge(age);
+    }
+
+    private void setName(String name) {
+        if (name.length() < 1) {
+            throw new IllegalArgumentException("Name cannot be empty.");
+        }
+        this.name = name;
+    }
+
+    private void setAge(int age) {
+        if (age < 0 || age > 15) {
+            throw new IllegalArgumentException("Age should be between 0 and 15.");
+        }
+        this.age = age;
+    }
+    public double productPerDay () {
+        return calculateProductPerDay();
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Chicken %s (age %d) can produce %.2f eggs per day."
+                , name , age , productPerDay());
+    }
+    private double calculateProductPerDay () {
+        double eggs = 0;
+        if (age > 0 && age <= 5) {
+            eggs = 2;
+        } else if (age >= 6 && age <= 11) {
+            eggs = 1;
+        } else if (age > 11) {
+            eggs = 0.75;
+        }
+        return eggs;
+    }
+}
---
+++ EncapsulationExercises/Problem2AnimalFarm/Main.java
@@ -0,0 +1,17 @@
+package EncapsulationExercises.Problem2AnimalFarm;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        String name = s.nextLine();
+        int age = Integer.parseInt(s.nextLine());
+
+        Chicken chicken = new Chicken(name,age);
+
+        System.out.println(chicken.toString());
+    }
+}
