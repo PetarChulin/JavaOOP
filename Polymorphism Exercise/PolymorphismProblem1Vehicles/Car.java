d72f6d84a06949d54f72d17f2f1e8cc3f2b789b2
PetarChulin <petercbg@mail.bg>
2022-03-09 13:45:10 +0200

    Update Car.java

--- Polymorphism Exercise/PolymorphismProblem1Vehicles/Car.java
+++ Polymorphism Exercise/PolymorphismProblem1Vehicles/Car.java
@@ -1,11 +1,11 @@
 package PolymorphismProblem1Vehicles;
 
 public class Car extends Vehicle {
-    private static final Double ADD_FUEL_CONSUME = 0.9;
+    private static final Double ADD_FUEL_CONSUMED = 0.9;
 
     public Car(Double fuelQuantity, Double fuelConsumption) {
         super(fuelQuantity, fuelConsumption);
-        setFuelConsumption(fuelConsumption + ADD_FUEL_CONSUME);
+        setFuelConsumption(fuelConsumption + ADD_FUEL_CONSUMED);
     }
     protected static Car makeCar(String[] carInfo) {
         return new Car(Double.parseDouble(carInfo[1]), Double.parseDouble(carInfo[2]));
