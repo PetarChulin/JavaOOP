01903e61d22ceeb9b5d21bd5bda8f40d4606f1b1
PetarChulin <petercbg@mail.bg>
2022-03-10 14:48:35 +0200

    Add files via upload

---
+++ Polymorphism Exercise/Problem4Word/CommandInterface.java
@@ -0,0 +1,6 @@
+package Problem4Word;
+
+public interface CommandInterface {
+    void init();
+    void handleInput(String input);
+}
---
+++ Polymorphism Exercise/Problem4Word/Initialization.java
@@ -0,0 +1,9 @@
+package Problem4Word;
+
+public class Initialization {
+    public static CommandInterface buildCommandInterface(StringBuilder text) {
+        CommandInterface commandInterface = new CommandImpl(text);
+        commandInterface.init();
+        return commandInterface;
+    }
+}
