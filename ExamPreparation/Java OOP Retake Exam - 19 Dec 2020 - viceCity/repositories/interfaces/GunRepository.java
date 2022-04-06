249013f6845ef0879a0f981d1ddd2ee1b027c858
PetarChulin <petercbg@mail.bg>
2022-04-06 09:27:22 +0300

    Add files via upload

---
+++ ExamPreparation/Java OOP Retake Exam - 19 Dec 2020 - viceCity/repositories/interfaces/GunRepository.java
@@ -0,0 +1,38 @@
+package viceCity.repositories.interfaces;
+
+import viceCity.models.guns.Gun;
+
+import java.util.*;
+
+public class GunRepository implements Repository<Gun> {
+
+
+
+    Map<String, Gun> models;
+
+    public GunRepository() {
+        models = new LinkedHashMap<>();
+    }
+
+    @Override
+    public Collection<Gun> getModels() {
+        return Collections.unmodifiableCollection(models.values());
+    }
+
+    @Override
+    public void add(Gun model) {
+        models.putIfAbsent(model.getName(), model);
+    }
+
+    @Override
+    public boolean remove(Gun model) {
+        return models.remove(model.getName()) != null;
+    }
+
+    @Override
+    public Gun find(String name) {
+        return models.get(name);
+    }
+
+
+}
