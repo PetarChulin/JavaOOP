47b48a3440c24e1d6b66229cabc28d25cad7563e
PetarChulin <petercbg@mail.bg>
2022-04-06 09:25:35 +0300

    Update LongHouse.java

--- ExamPreparation/Java OOP Exam - 11 December 2021 - catHouse/entities/houses/LongHouse.java
+++ ExamPreparation/Java OOP Exam - 11 December 2021 - catHouse/entities/houses/LongHouse.java
@@ -2,7 +2,7 @@ package catHouse.entities.houses;
 
 public class LongHouse extends BaseHouse{
 
-    private final static int CAPACITY = 15;
+    private final static int CAPACITY = 30;
 
 
     public LongHouse(String name) {
